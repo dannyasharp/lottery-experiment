6d899b6fbe5d45bdea5c8a315d808f826c447800
Danny Thesharps Mariah Sharp <thesharps@Dannys-MacBook-Pro-2.local>
2024-10-04 20:12:16 -0700

    fix redis URL protocol

--- server.js
+++ server.js
@@ -3,9 +3,58 @@ const app = express();
 const port = 3000;
 
 const { REDIS_URL } = process.env;
-console.log(REDIS_URL);
+const redis = require("redis");
+const client = redis.createClient({ url: REDIS_URL });
+const ulid = require("ulid");
+
+//this will write errors
+client.on("error", (error) => {
+	console.log(error);
+
+});
 
 app.use(express.json({ limit: '10kb' }));
+
+app.post("/lotteries", async (req, res) => {
+const { type, name, prize} = req.body;
+
+ if (type !== "simple") {
+ 	res.status(422).json({ error: "invalid lottery type"})
+ 	return;
+ }
+
+ if (typeof name !== "string" || name.length < 3) {
+ 	res.status(422).json({ error: "invalid lottery name"});
+ 	return;
+ }
+
+ if (typeof prize !== "string" || name.length < 3) {
+ 	res.status(422).json({ error: "Invalid lottery prize"});
+ 	return;
+ }
+
+ const id = ulid.ulid();
+ const newLottery = {
+ 	id,
+ 	name,
+ 	prize,
+ 	type,
+ 	status: "running"
+ };
+
+ try {
+	await client
+		.multi()
+		.hSet(`lottery.${id}`, newLottery)
+		.lPush("lotteries", id)
+	.exec();
+
+	res.json(newLottery);
+}	catch (error) {
+	console.error(error);
+	res.status(500).json({ error: "failed to create lottery"});
+}
+});
 
 app.get("/", (req, res) => {
 	// send an empty object as response
